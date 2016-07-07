327c2caf4084fc77e42968a72c0a3fdb115479af
Arcitectus <michael@xn--rtzel-gra.com>
2016-07-07 13:17:13 +0100

    16.06.06.13 draft for Squadron UI.

---
+++ src/Sanderling.Interface/Sanderling.Interface/MemoryStruct/SquadronUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Sanderling.Interface.MemoryStruct
+{
+	public interface SquadronsUI
+	{
+		IEnumerable<SquadronUI> SetSquadron { get; }
+
+		IUIElement LaunchAllButton { get; }
+
+		IUIElement OpenBayButton { get; }
+
+		IUIElement RecallAllButton { get; }
+	}
+
+	public interface SquadronUI
+	{
+		int? SquadronNumber { get; }
+
+		SquadronHealth Health { get; }
+
+		IEnumerable<SquadronAbility> SetAbility { get; }
+	}
+
+	public interface SquadronHealth
+	{
+		int? SquadronSizeMax { get; }
+
+		int? SquadronSizeCurrent { get; }
+	}
+
+	public interface SquadronAbility : IUIElement
+	{
+		int? Quantity { get; }
+	}
+}
